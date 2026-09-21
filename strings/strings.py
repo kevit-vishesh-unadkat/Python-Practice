86a7b65ad2d1afcf1f48bae355979ed482dc39f7
kevit-vishesh-unadkat <vishesh.unadkat@kevit.io>
2026-09-21 12:05:35 +0530

    feat(strings):explore string concept

--- strings/strings.py
+++ strings/strings.py
@@ -1,31 +1,42 @@
-# USER PROFILE 
+# Log Analyzer Applcation
 
-# in witch string concept is most time is used
-
-name="  vishesh unadkat  "
-# email="VISHESHUNADKAT@GMAIL.COM"
-email=input("enter your email")
-username="vishesh123"
-bio = "AI/ML enthusiast, Python developer, RAG learner"
-
-# validate a email
-
-def validate_email():
-    """
-    this function is for validate user email 
-    """
-
-    user_email=email.strip().lower()
-    print(user_email)
-
-    if "@" not in user_email:
-        print("Invalid Email:email do not have @")
-
-    if user_email.startswith("@"):
-        print("Invalid Email : email do not start with @")
-
-    if user_email.endswith("@"):
-        print("Invalid Email : email do not end with @")        
+logs=[
+    "2026-09-21 10:32:15 | INFO | user=vishesh | action=login | ip=192.168.1.10",
+    "2026-09-21 10:35:42 | ERROR | user=rahul | action=payment | amount=499 | error=Payment Failed",
+    "2026-09-21 10:36:10 | WARNING | user=amit | action=login | ip=192.168.1.25",
+    "2026-09-21 10:40:21 | INFO | user=vishesh | action=logout",
+    "2026-09-21 10:45:33 | ERROR | user=neha | action=payment | amount=999 | error=Card Declined",
+    "2026-09-21 10:50:12 | INFO | user=rahul | action=login | ip=192.168.1.30",
+    "2026-09-21 10:55:45 | WARNING | user=neha | action=login | ip=192.168.1.40",
+]
 
 
-validate_email()
+# show all logs 
+
+def show_all_logs():
+
+    for log in logs:
+        print(log)
+
+show_all_logs()
+
+
+# search a log  
+
+def search_log():
+    keyword=input("enter you what you want to search   : ").strip()
+
+    if "" in keyword:
+        print("keyword can not be empty")
+
+    found=False
+
+    for log in logs:
+        if keyword.lower()==log.lower():
+            print(log)
+            found=True
+
+    if not found:
+        print("No matching logs found")
+
+search_log()
